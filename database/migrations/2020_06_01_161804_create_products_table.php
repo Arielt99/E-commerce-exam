1504ae72dfe724266efe314ba9f12844c5675012
Arielt99 <arielthibault@yahoo.fr>
2020-06-01 19:28:40 +0200

    vue&vue router adding + migration + model starting

---
+++ database/migrations/2020_06_01_161804_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('principal_image');
+            $table->string('secondary_image');
+            $table->string('description');
+            $table->string('color');
+            $table->integer('brand_id')->unsigned();
+            $table->integer('price');
+            $table->boolean('isActive')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
